26829543676d56a2da190ae1dcffe131addccb3c
vadim8kiselev <vadim8kiselev@gmail.com>
2017-02-11 00:12:07 +0400

    Fix annotation configuration
    Signed-off-by: vadim8kiselev <vadim8kiselev@gmail.com>

--- dataframework-mapper/src/main/java/com/kiselev/dataframework/mapper/annotation/Table.java
+++ dataframework-mapper/src/main/java/com/kiselev/dataframework/mapper/annotation/Table.java
@@ -1,11 +1,18 @@
 package com.kiselev.dataframework.mapper.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @author Vadim Kiselev
  * @version 1.0
  * @since 02/10/2017
  */
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface Table {
 
     String tableName();
